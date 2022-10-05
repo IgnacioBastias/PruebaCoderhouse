226b53fa0dcbe4fbd4db9cae57d401133286aba0
IgnacioBastias <103468121+IgnacioBastias@users.noreply.github.com>
2022-10-05 17:38:58 -0300

    primer commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const logicaServer = (peticion, respuesta) => {
+	console.log('LLEGO UNA PETICION');
+	respuesta.end('Hola mundo')
+}
+
+const server = http.createServer(logicaServer)
+
+const connectedServer = server.listen(8080, () => {
+	console.log(`Servidor Http escuchando en el puerto ${connectedServer.address().port}`)
+})
